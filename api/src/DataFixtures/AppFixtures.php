7d872d25662945ec2af72f5160ef6e7989e68228
Ruben van der Linde <ruben@conduction.nl>
2020-04-07 08:22:42 +0200

    Fix on the app fixtures

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -2,34 +2,26 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Geboorte;
-use App\Entity\Ingeschrevenpersoon;
-use App\Entity\Kind;
-use App\Entity\NaamPersoon;
-use App\Entity\Ouder;
-use App\Entity\Partner;
-use App\Entity\Verblijfplaats;
-use App\Entity\Waardetabel;
 use DateTime;
+use Ramsey\Uuid\Uuid;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use PhpOffice\PhpSpreadsheet;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class AppFixtures extends Fixture
 {
     private $params;
-    private $encoder;
 
-    public function __construct(ParameterBagInterface $params, UserPasswordEncoderInterface $encoder)
+    public function __construct(ParameterBagInterface $params)
     {
         $this->params = $params;
-        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
     {
-       
+
     }
-    
-    
+
+
 }
