442363e96beb4b8fb161a0e8fe338a0277d18c9c
Ruben van der Linde <ruben@conduction.nl>
2020-05-06 07:13:36 +0200

    Added request roles

---
+++ api/src/Entity/Role.php
@@ -0,0 +1,205 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+
+/**
+ * @ApiResource()
+ *
+ * @ORM\Entity(repositoryClass="App\Repository\RoleRepository")
+ * @Gedmo\Loggable(logEntryClass="App\Entity\ChangeLog")
+ * @ORM\HasLifecycleCallbacks
+ *
+ * @ApiFilter(BooleanFilter::class)
+ * @ApiFilter(OrderFilter::class)
+ * @ApiFilter(DateFilter::class, strategy=DateFilter::EXCLUDE_NULL)
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "rolType": "exact",
+ *     "request": "exact",
+ *     "betrokkene": "exact",
+ *     "betrokkeneType": "exact"})
+ */
+
+class Role
+{
+    /**
+     * @var UuidInterface $id The UUID identifier of this resource
+     * @example e2984465-190a-4562-829e-a8cca81aa35d
+     *
+     * @Assert\Uuid
+     * @Groups({"read"})
+     * @ORM\Id
+     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\GeneratedValue(strategy="CUSTOM")
+     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+     */
+    private $id;
+
+    /**
+     * @var string $rolType The type of role
+     * @example initiator
+     *
+     * @Gedmo\Versioned
+     * @Assert\Choice({"initiator", "behandelaar", "belanghebbende"})
+     * @Assert\Length(
+     *      max = 255
+     * )
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $rolType;
+
+    /**
+     * @var string $request The spefic request that a person has a role on
+     *
+     * @Groups({"write"})
+     * @ORM\ManyToOne(targetEntity="App\Entity\Request", inversedBy="roles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $request;
+
+    /**
+     * @var string $betrokkene  A person that has a role, can be a User, Person or Employee object
+     * @example https://cc.zaakonline.nl/people/16353702-4614-42ff-92af-7dd11c8eef9f
+     *
+     * @Gedmo\Versioned
+     * @Assert\NotNull
+     * @Assert\Url
+     * @Groups({"read", "write"})
+     * @ORM\Column(type="string", length=255)
+     */
+    private $betrokkene;
+
+    /**
+     * @var string $betrokkeneType The type of the betrokkene on this request
+     * @example Natuurlijk persoon
+     *
+     * @Gedmo\Versioned
+     * @Assert\Choice({"Natuurlijk persoon", "niet natuurlijk persoon", "Organisatorische eenheid", "medewerker"})
+     * @Assert\Length(
+     *      max = 255
+     * )
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $betrokkeneType;
+
+    /**
+     * @var Datetime $dateCreated The moment this request was created
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateCreated;
+
+    /**
+     * @var Datetime $dateModified  The moment this request last Modified
+     *
+     * @Groups({"read"})
+     * @Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $dateModified;
+
+    public function getId():?Uuid
+    {
+        return $this->id;
+    }
+
+    public function setId($id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getRoleType(): ?string
+    {
+        return $this->roleType;
+    }
+
+    public function setRoleType(string $roleType): self
+    {
+        $this->roleType = $roleType;
+
+        return $this;
+    }
+
+    public function getRequest(): ?Request
+    {
+        return $this->request;
+    }
+
+    public function setRequest(?Request $request): self
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
+    public function getBetrokkene(): ?string
+    {
+        return $this->betrokkene;
+    }
+
+    public function setBetrokkene(string $betrokkene): self
+    {
+        $this->betrokkene  = betrokkene;
+
+        return $this;
+    }
+
+    public function getBetrokkeneType(): ?string
+    {
+        return $this->betrokkeneType;
+    }
+
+    public function setBetrokkeneType(string $betrokkeneType): self
+    {
+        $this->betrokkeneType = $betrokkeneType;
+
+        return $this;
+    }
+
+    public function getDateCreated(): ?\DateTimeInterface
+    {
+        return $this->dateCreated;
+    }
+
+    public function setDateCreated(\DateTimeInterface $dateCreated): self
+    {
+        $this->dateCreated= $dateCreated;
+
+        return $this;
+    }
+
+    public function getDateModified(): ?\DateTimeInterface
+    {
+        return $this->dateModified;
+    }
+
+    public function setDateModified(\DateTimeInterface $dateModified): self
+    {
+        $this->dateModified = $dateModified;
+
+        return $this;
+    }
+}
