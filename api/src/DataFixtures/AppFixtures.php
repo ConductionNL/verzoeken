a1001e3718983d1d113b9c3c2170ddb20a82db2b
Ruben van der Linde <rubenvdlinde@gmail.com>
2020-06-09 14:13:56 +0000

    Apply fixes from StyleCI

--- api/src/DataFixtures/AppFixtures.php
+++ api/src/DataFixtures/AppFixtures.php
@@ -6,8 +6,8 @@ use App\Entity\Request;
 use App\Entity\Submitter;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Ramsey\Uuid\Uuid;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class AppFixtures extends Fixture
 {
@@ -21,26 +21,25 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         // Lets make sure we only run these fixtures on larping enviroment
-        if ($this->params->get('app_domain') != "mijncluster.nl" && strpos($this->params->get('app_domain'), "mijncluster.nl") == false) {
+        if ($this->params->get('app_domain') != 'mijncluster.nl' && strpos($this->params->get('app_domain'), 'mijncluster.nl') == false) {
             return false;
         }
 
-        $now = New \Datetime;
+        $now = new \Datetime();
 
         $id = Uuid::fromString('456918bc-8419-4e54-90eb-bafd3d18c6ff');
         $request = new Request();
-        $request->setOrganization("https://wrc.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa");
-        $request->setRequestType("https://vtc.mijncluster.nl/request_types/23d4803a-67cd-4720-82d0-e1e0a776d8c4");
-        $request->setStatus("submited");
+        $request->setOrganization('https://wrc.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa');
+        $request->setRequestType('https://vtc.mijncluster.nl/request_types/23d4803a-67cd-4720-82d0-e1e0a776d8c4');
+        $request->setStatus('submited');
         $request->setDateSubmitted($now);
         $request->setProperties(
             [
-                "datum" =>$now->format('Y-m-d H:i:s'),
-                "adres" =>"Een willekeurig bag adres",
-                "persoon" =>"Een willekeurig bsn"
+                'datum'   => $now->format('Y-m-d H:i:s'),
+                'adres'   => 'Een willekeurig bag adres',
+                'persoon' => 'Een willekeurig bsn',
             ]
         );
-
 
         $manager->persist($request);
         $request->setId($id);
@@ -48,11 +47,11 @@ class AppFixtures extends Fixture
         $manager->flush();
         $request = $manager->getRepository('App:Request')->findOneBy(['id' => $id]);
 
-        $sumitter = New Submitter();
+        $sumitter = new Submitter();
         $sumitter->setRequest($request);
         $request->addSubmitter($sumitter);
-        $sumitter->setBrp("https://brp.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa");
-        $sumitter->setPerson("https://cc.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa");
+        $sumitter->setBrp('https://brp.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa');
+        $sumitter->setPerson('https://cc.mijncluster.nl/organizations/cc935415-a674-4235-b99d-0c7bfce5c7aa');
         $manager->persist($sumitter);
 
         $manager->flush();
