d266f37b8a9915d81312461755610cd941a623ef
Ruben van der Linde <ruben@conduction.nl>
2020-08-15 09:06:49 +0200

    Made order nullable

--- api/src/Entity/Request.php
+++ api/src/Entity/Request.php
@@ -106,10 +106,9 @@ class Request
      * @example https://wrc.zaakonline.nl/organisations/16353702-4614-42ff-92af-7dd11c8eef9f
      *
      * @Gedmo\Versioned
-     * @Assert\NotNull
      * @Assert\Url
      * @Groups({"read", "write"})
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $order;
 
